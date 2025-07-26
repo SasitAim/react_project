26cce20e65f0bd413cfcadffad3fa494e9f4b8e2
SasitAim <sasitpiasai02@gmail.com>
2025-07-26 10:01:05 +0700

    change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header02 from './components/Header02';
 import Footer from './components/Footer';
 import Home from './Pages/Home';
